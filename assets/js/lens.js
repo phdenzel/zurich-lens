4a853894c88ad64e6a104acd0c81eab54adf54b8
phdenzel <phdenzel@gmail.com>
2018-07-31 16:25:56 +0200

    Rewrote lensing.js: cleaner, better, javascriptier

---
+++ assets/js/lens.js
@@ -0,0 +1,28 @@
+// Grabbing settings
+var ww = window.innerWidth;
+var wh = window.innerHeight;
+var dst = document.getElementById('dst');
+var img = new Image();
+img.src = "assets/images/zurich.jpg"
+
+
+function loadSrc(resolve) {
+    console.log(resolve);
+    LensModule.readImage(img);
+}
+
+
+function loop() {
+    // console.log(LensModule);
+    LensModule.startLoop();
+    
+}
+
+
+LensModule.init({
+    imageCvs: dst,
+    windowWidth: ww,
+    windowHeight: wh,
+    initSuccessCallback: loadSrc,
+    readCallback: loop
+});
--- assets/js/lensing.js
+++ assets/js/lensing.js
@@ -1,186 +1,273 @@
-// CERN lensing
-// image data
-var imageDataDst, imageDataSrc;
+var LensModule = (function() {
 
-// temporary postion variables
-var oldx = 0;
-var oldy = 0;
+    var imageCvs;            // canvas for lens processing
+    var imageCtx;            // context of processing canvas
+    var imageDataSrc;        // original canvas image data
+    var imageDataDst;        // image data for processing
+    var imageWidth;          // image width
+    var imageHeight;         // image height
+    var windowWidth;         // width of the window embedding canvas
+    var windowHeight;        // height of the window embedding canvas
 
-// linear interpolation
-var lerp = function(v1, v2, t) {
-    // 
-    return (v2 - v1) * (1-Math.exp(-t)) + v1;
-};
+    var initSuccessCallback; // called after init function succeeds
+    var initErrorCallback;   // called if init function causes error
+    var readCallback;        // called after readImg read functions
 
-// mouse tracking
-function getMousePos(evt) {
+    var lensType;            // what kind of lens should be applied
+    var radius;              // lensing influence radius
+    var center = {};         // center of lens
+    var tolerance;           // tolerance
 
-    return { // problems with floats, use rather integers
-        x: (evt.clientX / $(window).width() * canvas.width) >> 0,
-        y: (evt.clientY / $(window).height() * canvas.height) >> 0
-    };
-};
 
-// custom event listener
-function lensingListener(evt){
-    var mousePos = getMousePos(evt);
-    console.log(mousePos);
-    updatecanvas(canvas, mousePos.x, mousePos.y);
-};
+    function init(options) {
 
-// apply lensing manipulation
-window.onload = function() {
-
-    canvas = document.querySelector("canvas");
-
-    w = img.width;
-    h = img.height;
-
-    canvas.width = w;
-    canvas.height = h;
-
-    dst = canvas.getContext("2d");
-
-    dst.drawImage(img, 0, 0, w, h);
-    i = 0;
-    imageDataSrc = dst.getImageData(0, 0, w, h);
-    imageDataDst = dst.getImageData(0, 0, w, h);
-
-    px = 0;
-    py = h/2;
-
-    ti = 0;
-    var timer = setInterval(function() {
-        // if (ti++ > 100){
-        clearInterval(timer);
-        document.addEventListener('mousemove', lensingListener, false);
-		    // }
-
-        // updatecanvas(canvas, lerp(0, w*0.5, ti*0.05), py);
-
-    }, 16);
-
-};
-
-// actual lensing math
-function updatecanvas(canvas, px, py) {
-    var context = canvas.getContext('2d');
-
-    // define region where lensing is applied
-    r = 200;
-
-    xmin = Math.max(oldx-r, 0);
-    xmax = Math.min(oldx+r, w);
-    ymin = Math.max(oldy-r, 0);
-    ymax = Math.min(oldy+r, h);
-
-    // reload unlensed image
-    var halfindex;
-    for (y = ymin; y < ymax; y++) {
-        halfindex = y * w;
-        for (x = xmin; x < xmax; x++) {
-            index = (x + halfindex) << 2;
-            imageDataDst.data[index] = imageDataSrc.data[index++]; // r
-            imageDataDst.data[index] = imageDataSrc.data[index++]; // g
-            imageDataDst.data[index] = imageDataSrc.data[index++]; // b
-            imageDataDst.data[index] = 255;                        // a
-        }
+        defaults(options)
+            .then(initSuccessCallback)
+            .catch(initErrorCallback)
     }
 
-    // buffer Uint8ClampedArrays
-    var dstdata = imageDataDst.data;
-    var srcdata = imageDataSrc.data;
 
-    xmin = Math.max(px-r, 0);
-    xmax = Math.min(px+r, w);
-    ymin = Math.max(py-r, 0);
-    ymax = Math.min(py+r, h);
+    function defaults(options) {
+        // initialize defaults and options
+        return new Promise( (resolve, reject) => {
+            if (!options) { //sanity check
+                reject('No data for lensing provided');
+            }
 
-    var tol = -15;
-    var maxSize = w * (h - 1) + w - 1;
+            //// default settings
+            imageCvs = options.imageCvs || document.createElement('canvas');
+            imageWidth = options.imageWidth || 1280;
+            imageHeight = options.imageHeight || 720;
+            windowWidth = options.windowWidth || window.innerWidth;
+            windowHeight = options.windowHeight || window.innerHeight;
+            // callbacks
+            initSuccessCallback = options.initSuccessCallback || function(resolve) {console.log(resolve)};
+            initErrorCallback = options.initErrorCallback || function(error) {console.log(error)};
+            readCallback = options.readCallback || function() {};
+            // lens properties
+            lensType = options.lensType || 'isothermal';
+            radius = options.radius || 200;
+            center = options.center || {x: 0, y: 0};
+            tolerance = options.tolerance || -15;
 
-    // apply lens in defined range
-    for (y = ymin; y < ymax; y++) {
-        index = (xmin + y * w) << 2;
-        y1 = y - py;
-        for (x = xmin; x < xmax; x++) {
-            x1 = x - px;
-            d = Math.sqrt(x1 * x1 + y1 * y1);
-            if (d <= r) {
-                // lensing math
-                // sc = 0; // no lensing
-                sc = 200*(1-Math.tanh(3*d/r))/(d+1);  // smooth transition to sc=0 at r
-                // isothermal lensing equation a la beta = theta - theta_E/theta
-                xx = Math.floor(x - x1 * sc); 
-                yy = Math.floor(y - y1 * sc);
+            //// setting up image components
+            imageCvs.width = imageWidth;
+            imageCvs.height = imageHeight;
+            imageCtx = imageCvs.getContext('2d');
 
-                // Antialiasing, i.e. preserve brightness more or less
-                if (sc < tol * 0.9 && sc > tol * 1.1)
-                    sc = 0.9;
-                else if (sc < tol)
-                    sc = 0.1;
-                else
-                    sc = 1;
-                // end of lensing math
+            setTimeout(function() {
+                resolve("LensModule configured");
+            }, 1000);
+        });
+    }
 
-                index2 = ((xx + yy * w) % maxSize) << 2;
-                dstdata[index++] = sc * srcdata[index2 + 0]; // r
-                dstdata[index++] = sc * srcdata[index2 + 1]; // g
-                dstdata[index++] = sc * srcdata[index2 + 2]; // b
-                index++;
-            } else {
-                index = index + 4;
+
+    function readImage(srcImg) {
+        imageCtx.drawImage(srcImg, 0, 0, imageWidth, imageHeight);
+        imageDataSrc = imageCtx.getImageData(0, 0, imageWidth, imageHeight); // this will not change until readImage is called again!
+        imageDataDst = imageCtx.getImageData(0, 0, imageWidth, imageHeight);
+        readCallback();
+    }
+
+
+    function loadImageData(srcImageData) {
+        readCallback();
+    }
+
+
+    function source(px, py, dstID, srcID) {
+        // put the source image data back into destination image data at (px, py)
+        dstID = dstID || imageDataDst;
+        srcID = srcID || imageDataSrc;
+        
+        // at (px, py) around the lensing radius
+        var xmin, xmax, ymin, ymax;
+        var x, y, halfidx, idxDst;
+        
+        // setup deflection range
+        xmin = Math.max(px-radius, 0);
+        xmax = Math.min(px+radius, imageWidth);
+        ymin = Math.max(py-radius, 0);
+        ymax = Math.min(py+radius, imageHeight);
+        
+        // reset to original data
+        for (y = ymin; y < ymax; y++) {
+            halfidx = y * imageWidth;
+            for (x = xmin; x < xmax; x++) {
+                idxDst = (x + halfidx) << 2;
+                dstID.data[idxDst] = srcID.data[idxDst++]; // r
+                dstID.data[idxDst] = srcID.data[idxDst++]; // g
+                dstID.data[idxDst] = srcID.data[idxDst++]; // b
+                dstID.data[idxDst] = 255;                  // a
             }
         }
     }
 
-    // write from buffer
-    imageDataDst.data = dstdata;
-    dst.putImageData(imageDataDst, 0, 0);
-    oldx = px;
-    oldy = py;
-};
 
-function clearcanvas(){
-  canvas = document.querySelector("canvas");
-  var context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  // var w = canvas.width;
-  // canvas.width = 1;
-  // canvas.width = w;
-};
+    function process(px, py, dstID, srcID) {
+        // let LensModule process image data with new center at (px, py)
+        dstID = dstID || imageDataDst;
+        srcID = srcID || imageDataSrc;
 
-function load_lensing(){
-    canvas = document.querySelector("canvas");
+        // Uint8ClampedArray data buffers
+        var dstData = dstID.data;
+        var srcData = srcID.data;
+        
+        switch (lensType) {
+        case 'isothermal':
+            dstData = isothermLens(dstData, srcData, px, py);
+            break;
+        case 'point':
+            dstData = pointLens(dstData, srcData, px, py);
+            break;
+        default:
+            dstData = isotherm_lens(dstData, srcData, px, py);   
+        }
 
-    w = img.width;
-    h = img.height;
+        dstID.data = dstData;
+    }
 
-    canvas.width = w;
-    canvas.height = h;
 
-    dst = canvas.getContext("2d");
+    function isothermLens(lensData, unlensedData, px, py) {
+        // lens applicator following isothermal lens equation
+        // with smooth transition from center to radius
+        var distance, deflection, maxSize;
+        var x, y, idxDst, idxSrc, xbuf, ybuf, dx, dy;
+        var xmin, xmax, ymin, ymax;
 
-    dst.drawImage(img, 0, 0, w, h);
-    i = 0;
-    imageDataSrc = dst.getImageData(0, 0, w, h);
-    imageDataDst = dst.getImageData(0, 0, w, h);
 
-    px = 0;
-    py = h/2;
+        // setup deflection range
+        xmin = Math.max(px-radius, 0);
+        xmax = Math.min(px+radius, imageWidth);
+        ymin = Math.max(py-radius, 0);
+        ymax = Math.min(py+radius, imageHeight);
 
-    ti = 0;
-    var timer = setInterval(function() {
-        if (ti++ > 100){
-            clearInterval(timer);
-            document.addEventListener('mousemove', lensingListener, false);
-		    }
+        maxSize = imageWidth * (imageHeight - 1) + imageWidth - 1;
 
-        updatecanvas(canvas, lerp(0, w*0.5, ti*0.05), py);
+        for (y = ymin; y < ymax; y++) {
+            idxDst = (xmin + y * imageWidth) << 2;
+            ybuf = y - py;
+            for (x = xmin; x < xmax; x++) {
+                xbuf = x - px;
+                distance = Math.sqrt(xbuf*xbuf + ybuf*ybuf);
+                if (distance <= radius) {
+                    // calculate deflection indices
+                    deflection = radius * (1 - Math.tanh(4*distance/radius))/(distance+1);
 
-    }, 16);
-};
+                    // actual lensing equation
+                    dx = Math.floor(x - xbuf * deflection);
+                    dy = Math.floor(y - ybuf * deflection);
 
-// create Image
-var img = new Image();
-img.src = "assets/images/zurich.jpg";
+                    // antialiasing (while preserving brightness more or less)
+                    if (deflection < tolerance * 0.9 && deflection > tolerance * 1.1) {
+                        deflection = 0.9;
+                    } else if (deflection < tolerance) {
+                        deflection = 0.1;
+                    } else {
+                        deflection = 1;
+                    }
+
+                    // move pixels
+                    idxSrc = ((dx + dy * imageWidth) % maxSize) << 2;
+                    lensData[idxDst++] = deflection * unlensedData[idxSrc + 0]; // r
+                    lensData[idxDst++] = deflection * unlensedData[idxSrc + 1]; // g
+                    lensData[idxDst++] = deflection * unlensedData[idxSrc + 2]; // b
+                    idxDst++; // leave alpha unchanged
+                } else {
+                    idxDst = idxDst + 4;
+                }
+            }
+        }
+
+        return lensData;
+    }
+
+
+    function pointLens(lensData, unlensedData, px, py) {
+        return lensData
+    }
+    
+
+    function startLoop() {
+        imageCvs.addEventListener('mousemove', listener, false);
+    }
+
+ 
+    function listener(evt) {
+        var mousePosition = trackMouse(evt);
+        source(center.x, center.y);
+        process(mousePosition.x, mousePosition.y);
+        imageCtx.putImageData(imageDataDst, 0, 0);
+        setCenter(mousePosition.x, mousePosition.y);
+        
+    }
+
+    
+    function trackMouse(evt) {
+        return {
+            x: (evt.clientX / windowWidth  * imageWidth)  >> 0,
+            y: (evt.clientY / windowHeight * imageHeight) >> 0
+        };
+    }
+
+
+
+
+    // getters and setters
+    function getLensType() {
+        return lensType;
+    }
+    
+    function setLensType(type) {
+        lensType = type;
+    }
+
+    
+    function getRadius() {
+        return radius;
+    }
+
+    function setRadius(val) {
+        radius = val;
+    }
+
+    
+    function getCenter() {
+        return center;
+    }
+
+    function setCenter(x, y) {
+        center = {x: x, y: y};
+    }
+
+
+    function getTolerance() {
+        return tolerance;
+    }
+
+    function setTolerance(tol) {
+        tolerance = tol;
+    }
+
+
+    return {
+
+        // getters/setters
+        getLensType: getLensType,
+        setLensType: setLensType,
+        getRadius: getRadius,
+        setRadius: setRadius,
+        getCenter: getCenter,
+        setCenter: setCenter,
+        getTolerance: getTolerance,
+        setTolerance: setTolerance,
+
+        // functions
+        init,
+        readImage,
+        source,
+        process,
+        startLoop
+
+    };
+})();
